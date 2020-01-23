LucasCCabral/knot-thing-lib-android

Add register operation
Add register-thing operation using AMQP.

knot-thing/src/main/java/com/cesar/knot_sdk/KNoTAMQPController.kt
@@ -1,0 +1,19 @@
+package com.cesar.knot_sdk
+
+import com.cesar.knot_sdk.knot_messages.KNoTThingRegister
+import com.google.gson.Gson
+
+/**
+ * This class implements the KNoT-Protocol operations for AMQP.
+ */
+class KNoTAMQPController( val kNoTAMQP: KNoTAMQP) : KNoTMessager {
+
+    override fun register( knotThingRegister : KNoTThingRegister) {
+        kNoTAMQP.publish(
+            Gson().toJson(knotThingRegister),
+            kNoTAMQP.EXCHANGE_NAME_CLOUD,
+            kNoTAMQP.BINDING_KEY_REGISTER
+        )
+    }
+
+}

CESARBR/knot-android-sdk

knot-thing/src/main/java/com/cesar/knot_sdk/KNoTAMQPController.kt
@@ -1,0 +1,19 @@
+package com.cesar.knot_sdk
+
+import com.cesar.knot_sdk.knot_messages.KNoTMessageRegister
+import com.google.gson.Gson
+
+/**
+ * This class implements the KNoT-Protocol operations for AMQP.
+ */
+class KNoTAMQPController(val knotAMQP: KNoTAMQP) : KNoTMessager {
+
+    override fun register(knotThingRegister : KNoTMessageRegister) {
+        knotAMQP.publish(
+            Gson().toJson(knotThingRegister),
+            knotAMQP.EXCHANGE_NAME_CLOUD,
+            knotAMQP.BINDING_KEY_REGISTER
+        )
+    }
+
+}