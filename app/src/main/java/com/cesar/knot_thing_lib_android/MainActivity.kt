LucasCCabral/knot-thing-lib-android

Add unregister operation
Add unregister operation for AMQP.

app/src/main/java/com/cesar/knot_thing_lib_android/MainActivity.kt
@@ -6,7 +6,9 @@ import com.cesar.knot_sdk.KNoTAMQP
 import com.cesar.knot_sdk.KNoTAMQPFactory
 import com.cesar.knot_sdk.KNoTMessager
 import com.cesar.knot_sdk.knot_messages.KNoTThingRegister
+import com.cesar.knot_sdk.knot_messages.KNoTThingUnregister
 import kotlinx.android.synthetic.main.activity_main.register_button
+import kotlinx.android.synthetic.main.activity_main.unregister_button
 import org.jetbrains.anko.doAsync
 
 class MainActivity : AppCompatActivity() {
@@ -25,9 +27,14 @@ class MainActivity : AppCompatActivity() {
         val setKNoTMessager = { kNoTMessagerAux : KNoTMessager -> kNoTMessager = kNoTMessagerAux }
         KNoTAMQPFactory().getKNoTProtocolMessager(kNoTAMQP, setKNoTMessager)
         val kNoTThingRegister = KNoTThingRegister(THING_ID, THING_NAME)
+        val kNoTThingUnregister = KNoTThingUnregister(THING_ID)
 
         register_button.setOnClickListener {
             doAsync { kNoTMessager.register(kNoTThingRegister) }
+        }
+
+        unregister_button.setOnClickListener {
+            doAsync { kNoTMessager.unregister(kNoTThingUnregister) }
         }
     }
 }

knot-thing/src/main/java/com/cesar/knot_sdk/KNoTAMQPController.kt
@@ -1,6 +1,7 @@
 package com.cesar.knot_sdk
 
 import com.cesar.knot_sdk.knot_messages.KNoTThingRegister
+import com.cesar.knot_sdk.knot_messages.KNoTThingUnregister
 import com.google.gson.Gson
 
 /**
@@ -13,6 +14,14 @@ class KNoTAMQPController( val kNoTAMQP: KNoTAMQP) : KNoTMessager {
             Gson().toJson(knotThingRegister),
             kNoTAMQP.EXCHANGE_NAME_CLOUD,
             kNoTAMQP.BINDING_KEY_REGISTER
+        )
+    }
+
+    override fun unregister(kNoTThingUnregister: KNoTThingUnregister) {
+        kNoTAMQP.publish(
+            Gson().toJson(kNoTThingUnregister),
+            kNoTAMQP.EXCHANGE_NAME_CLOUD,
+            kNoTAMQP.BINDING_KEY_UNREGISTER
         )
     }
 

CESARBR/knot-android-sdk

app/src/main/java/com/cesar/knot_thing_lib_android/MainActivity.kt
@@ -6,7 +6,9 @@ import com.cesar.knot_sdk.KNoTAMQP
 import com.cesar.knot_sdk.KNoTAMQPFactory
 import com.cesar.knot_sdk.KNoTMessager
 import com.cesar.knot_sdk.knot_messages.KNoTMessageRegister
+import com.cesar.knot_sdk.knot_messages.KNoTMessageUnregister
 import kotlinx.android.synthetic.main.activity_main.register_button
+import kotlinx.android.synthetic.main.activity_main.unregister_button
 import org.jetbrains.anko.doAsync
 
 class MainActivity : AppCompatActivity() {
@@ -24,9 +26,14 @@ class MainActivity : AppCompatActivity() {
         lateinit var knotMessager : KNoTMessager
         KNoTAMQPFactory().create(knotAMQP)
         val knotThingRegister = KNoTMessageRegister(THING_ID, THING_NAME)
+        val knotThingUnregister = KNoTMessageUnregister(THING_ID)
 
         register_button.setOnClickListener {
             doAsync { knotMessager.register(knotThingRegister) }
+        }
+
+        unregister_button.setOnClickListener {
+            doAsync { knotMessager.unregister(knotThingUnregister) }
         }
     }
 }

knot-thing/src/main/java/com/cesar/knot_sdk/KNoTAMQPController.kt
@@ -1,6 +1,7 @@
 package com.cesar.knot_sdk
 
 import com.cesar.knot_sdk.knot_messages.KNoTMessageRegister
+import com.cesar.knot_sdk.knot_messages.KNoTMessageUnregister
 import com.google.gson.Gson
 
 /**
@@ -13,6 +14,14 @@ class KNoTAMQPController(val knotAMQP: KNoTAMQP) : KNoTMessager {
             Gson().toJson(knotThingRegister),
             knotAMQP.EXCHANGE_NAME_CLOUD,
             knotAMQP.BINDING_KEY_REGISTER
+        )
+    }
+
+    override fun unregister(knotThingUnregister : KNoTMessageUnregister) {
+        knotAMQP.publish(
+            Gson().toJson(knotThingUnregister),
+            knotAMQP.EXCHANGE_NAME_CLOUD,
+            knotAMQP.BINDING_KEY_UNREGISTER
         )
     }
 